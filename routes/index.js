2de5d1571c20227fdf4bd9bba1d6cbf5a95ec08d
Mihir <mihir.sstech@gmail.com>
2024-03-18 11:10:45 +0530

    👍

--- routes/index.js
+++ routes/index.js
@@ -6,18 +6,12 @@ const userRoutes = require('./userRoutes');
 const servicesRoutes = require('./servicesRoutes');
 const aboutRoutes = require('./aboutRoutes');
 const dashboardRoutes = require('./dashboardRoutes');
-<<<<<<< HEAD
 const faqRoutes = require('./faqRoutes');
-=======
->>>>>>> c592d776056d913db3f98b4da90399b0754c7119
-
 // Use route handlers
 router.use(userRoutes);
 router.use(servicesRoutes);
 router.use(aboutRoutes);
 router.use(dashboardRoutes);
-<<<<<<< HEAD
 router.use(faqRoutes);
-=======
->>>>>>> c592d776056d913db3f98b4da90399b0754c7119
+
 module.exports = router;
